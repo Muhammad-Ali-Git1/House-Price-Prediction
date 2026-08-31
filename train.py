3ca6dacb6a2d2296a0a179fe0b2173800303fcde
Muhammad Ali <malipk526@gmail.com>
2026-08-31 16:42:14 +0500

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 # Load dataset
-df = pd.read_csv("housing.csv")
+df = pd.read_pickle("housing.pkl")
 
 # Remove null values
 df.dropna(inplace=True)
